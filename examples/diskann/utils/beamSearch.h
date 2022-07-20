c81d3cde5c933ebe2938243267ba2983f6c1f34f
Magdalen Dobson <magdalen@aware.aladdin.cs.cmu.edu>
2022-07-20 16:10:09 -0400

    added diskann

---
+++ examples/diskann/utils/beamSearch.h
@@ -0,0 +1,364 @@
+// This code is part of the Problem Based Benchmark Suite (PBBS)
+// Copyright (c) 2011 Guy Blelloch and the PBBS team
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights (to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#ifndef BEAMSEARCH
+#define BEAMSEARCH
+
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+//#include "common/geometry.h"
+#include "parlay/parallel.h"
+#include "parlay/primitives.h"
+#include "parlay/random.h"
+#include "types.h"
+//#include "clustertree.h"
+#include "indexTools.h"
+
+extern bool report_stats;
+
+using pid = std::pair<int, float>;
+
+// returns true if F \setminus V = emptyset
+bool intersect_nonempty(parlay::sequence<pid>& V, parlay::sequence<pid>& F) {
+  for (int i = 0; i < F.size(); i++) {
+    auto pred = [&](pid a) { return F[i].first == a.first; };
+    if (parlay::find_if(V, pred) == V.end()) return true;
+  }
+  return false;
+}
+
+// will only be used when there is an element in F that is not in V
+// hence the ``return 0" line will never be called
+pid id_next(parlay::sequence<pid>& V, parlay::sequence<pid>& F) {
+  for (int i = 0; i < F.size(); i++) {
+    auto pred = [&](pid a) { return F[i].first == a.first; };
+    if (parlay::find_if(V, pred) == V.end()) return F[i];
+  }
+  return std::make_pair(0, 0);
+}
+
+// for debugging
+void print_seq(parlay::sequence<int> seq) {
+  int fsize = seq.size();
+  std::cout << "[";
+  for (int i = 0; i < fsize; i++) {
+    std::cout << seq[i] << ", ";
+  }
+  std::cout << "]" << std::endl;
+}
+
+// takes in two sorted sequences and returns a sorted union
+template <typename Seq>
+parlay::sequence<pid> seq_union(Seq& P,
+                                parlay::sequence<pid>& Q) {
+  auto less = [&](pid a, pid b) { return a.second < b.second; };
+  pid* first1 = P.begin();
+  pid* last1 = P.end();
+  pid* first2 = Q.begin();
+  pid* last2 = Q.end();
+  parlay::sequence<pid> result = parlay::sequence<pid>();
+  result.reserve(P.size()+Q.size());
+  while (true) {
+    if (first1 == last1) {
+      while (first2 != last2) {
+        result.push_back(*first2);
+        ++first2;
+      }
+      return result;
+    } else if (first2 == last2) {
+      while (first1 != last1) {
+        result.push_back(*first1);
+        ++first1;
+      }
+      return result;
+    }
+    if (less(*first1, *first2)) {
+      result.push_back(*first1);
+      ++first1;
+    } else if (less(*first2, *first1)) {
+      result.push_back(*first2);
+      ++first2;
+    } else {
+      if (first1->first == first2->first) {
+        result.push_back(*first1);
+        ++first1;
+        ++first2;
+      } else {
+        result.push_back(*first1);
+        result.push_back(*first2);
+        ++first1;
+        ++first2;
+      }
+    }
+  }
+  return result;
+}
+
+// updated version by Guy
+template <typename T>
+std::pair<parlay::sequence<pid>, parlay::sequence<pid>> beam_search(
+    Tvec_point<T>* p, parlay::sequence<Tvec_point<T>*>& v,
+    Tvec_point<T>* medoid, int beamSize, unsigned d) {
+  // initialize data structures
+  std::vector<pid> visited;
+  parlay::sequence<pid> frontier;
+  auto less = [&](pid a, pid b) {
+      return a.second < b.second || (a.second == b.second && a.first < b.first); };
+  auto make_pid = [&] (int q) {
+    return std::pair{q, distance(v[q]->coordinates.begin(), p->coordinates.begin(), d)};};
+  int bits = std::ceil(std::log2(beamSize*beamSize));
+  parlay::sequence<int> hash_table(1 << bits, -1);
+
+  // the frontier starts with the medoid
+  frontier.push_back(make_pid(medoid->id));
+
+  std::vector<pid> unvisited_frontier(beamSize);
+  parlay::sequence<pid> new_frontier(2*beamSize);
+  unvisited_frontier[0] = frontier[0];
+  bool not_done = true;
+
+  // terminate beam search when the entire frontier has been visited
+  while (not_done) {
+    // the next node to visit is the unvisited frontier node that is closest to p
+    pid currentPid = unvisited_frontier[0];
+    Tvec_point<T>* current = v[currentPid.first];
+    auto g = [&] (int a) {
+	       int loc = parlay::hash64_2(a) & ((1 << bits) - 1);
+	       if (hash_table[loc] == a) return false;
+	       hash_table[loc] = a;
+	       return true;};
+    auto candidates = parlay::filter(current->out_nbh.cut(0,size_of(current->out_nbh)), g);
+    auto pairCandidates = parlay::map(candidates, [&] (long c) {return make_pid(c);});
+    auto sortedCandidates = parlay::unique(parlay::sort(pairCandidates, less));
+    auto f_iter = std::set_union(frontier.begin(), frontier.end(),
+				 sortedCandidates.begin(), sortedCandidates.end(),
+				 new_frontier.begin(), less);
+    size_t f_size = std::min<size_t>(beamSize, f_iter - new_frontier.begin());
+    frontier = parlay::tabulate(f_size, [&] (long i) {return new_frontier[i];});
+    visited.insert(std::upper_bound(visited.begin(), visited.end(), currentPid, less), currentPid);
+    auto uf_iter = std::set_difference(frontier.begin(), frontier.end(),
+				 visited.begin(), visited.end(),
+				 unvisited_frontier.begin(), less);
+    not_done = uf_iter > unvisited_frontier.begin();
+  }
+  return std::make_pair(frontier, parlay::to_sequence(visited));
+}
+
+template <typename T>
+std::pair<parlay::sequence<pid>, parlay::sequence<pid>> beam_search_prev(
+    Tvec_point<T>* p, parlay::sequence<Tvec_point<T>*>& v,
+    Tvec_point<T>* medoid, int beamSize, unsigned d) {
+  // initialize data structures
+  parlay::sequence<pid> visited = parlay::sequence<pid>();
+  visited.reserve(5*beamSize);
+  parlay::sequence<pid> frontier = parlay::sequence<pid>();
+  frontier.reserve(2*beamSize);
+  auto less = [&](pid a, pid b) { return a.second < b.second; };  
+  // the frontier starts with the medoid
+  frontier.push_back(std::make_pair(
+      medoid->id,
+      distance(medoid->coordinates.begin(), p->coordinates.begin(), d)));
+  // terminate beam search when the entire frontier has been visited
+  while (intersect_nonempty(visited, frontier)) {
+    // the next node to visit is the unvisited frontier node that is closest to
+    // p
+    pid currentPid = (id_next(visited, frontier));
+    Tvec_point<T>* current = v[currentPid.first];
+    auto g = [&](int a) {
+      auto pred = [&](pid p) { return p.first == a; };
+      if (parlay::find_if(visited, pred) == visited.end()) return true;
+      return false;
+    };
+    parlay::sequence<int> candidatesTemp = parlay::tabulate(size_of(current->out_nbh), [&] (size_t i) {
+      return current->out_nbh[i];});
+    auto candidates = parlay::filter(candidatesTemp, g);
+    parlay::sequence<pid> pairCandidates =
+        parlay::sequence<pid>(candidates.size());
+    for (int i = 0; i < candidates.size(); i++) {
+      pairCandidates[i] = std::make_pair(
+          candidates[i], distance(v[candidates[i]]->coordinates.begin(),
+                                  p->coordinates.begin(), d));
+    }
+    auto sortedCandidates = parlay::sort(pairCandidates, less);
+    frontier = seq_union(frontier, sortedCandidates);
+    if (frontier.size() > beamSize)
+      frontier.erase(frontier.begin() + beamSize, frontier.end());
+    // add the node to the visited list
+    visited.push_back(currentPid);
+
+  }
+  return std::make_pair(frontier, visited);
+}
+
+// searches every element in q starting from a randomly selected point
+template <typename T>
+void beamSearchRandom(parlay::sequence<Tvec_point<T>*>& q,
+                      parlay::sequence<Tvec_point<T>*>& v, int beamSizeQ, int k,
+                      unsigned d) {
+  if ((k + 1) > beamSizeQ) {
+    std::cout << "Error: beam search parameter Q = " << beamSizeQ
+              << " same size or smaller than k = " << k << std::endl;
+    abort();
+  }
+  // use a random shuffle to generate random starting points for each query
+  size_t n = v.size();
+  auto indices =
+      parlay::random_permutation<int>(static_cast<int>(n), time(NULL));
+  parlay::parallel_for(0, q.size(), [&](size_t i) {
+    parlay::sequence<int> neighbors = parlay::sequence<int>(k);
+    Tvec_point<T>* start = v[indices[i]];
+    parlay::sequence<pid> beamElts;
+    parlay::sequence<pid> visitedElts;
+    std::pair<parlay::sequence<pid>, parlay::sequence<pid>> pairElts;
+    pairElts = beam_search(q[i], v, start, beamSizeQ, d);
+    beamElts = pairElts.first;
+    visitedElts = pairElts.second;
+    // the first element of the frontier may be the point itself
+    // if this occurs, do not report it as a neighbor
+    if (beamElts[0].first == i) {
+      for (int j = 0; j < k; j++) {
+        neighbors[j] = beamElts[j + 1].first;
+      }
+    } else {
+      for (int j = 0; j < k; j++) {
+        neighbors[j] = beamElts[j].first;
+      }
+    }
+    q[i]->ngh = neighbors;
+    if (report_stats) q[i]->cnt = visitedElts.size();
+  });
+}
+
+template <typename T>
+void searchFromSingle(parlay::sequence<Tvec_point<T>*>& q,
+                      parlay::sequence<Tvec_point<T>*>& v, int beamSizeQ, int k,
+                      unsigned d, Tvec_point<T>* medoid) {
+  if ((k + 1) > beamSizeQ) {
+    std::cout << "Error: beam search parameter Q = " << beamSizeQ
+              << " same size or smaller than k = " << k << std::endl;
+    abort();
+  }
+  parlay::parallel_for(0, q.size(), [&](size_t i) {
+    parlay::sequence<int> neighbors = parlay::sequence<int>(k);
+    parlay::sequence<pid> beamElts;
+    parlay::sequence<pid> visitedElts;
+    std::pair<parlay::sequence<pid>, parlay::sequence<pid>> pairElts;
+    pairElts = beam_search(q[i], v, medoid, beamSizeQ, d);
+    beamElts = pairElts.first;
+    visitedElts = pairElts.second;
+    // the first element of the frontier may be the point itself
+    // if this occurs, do not report it as a neighbor
+    if (beamElts[0].first == i) {
+      for (int j = 0; j < k; j++) {
+        neighbors[j] = beamElts[j + 1].first;
+      }
+    } else {
+      for (int j = 0; j < k; j++) {
+        neighbors[j] = beamElts[j].first;
+      }
+    }
+    q[i]->ngh = neighbors;
+    if (report_stats) q[i]->cnt = visitedElts.size();
+  });
+}
+
+
+#endif
+
+// // Uses a std::set for the frontier and beam, and unordered_set for
+// // visited elements.
+// // NOT CURRENTLY USED ANYWHERE
+// template <typename T>
+// parlay::sequence<pid> beam_search_2(Tvec_point<T>* p,
+//                                     parlay::sequence<Tvec_point<T>*>& v,
+//                                     Tvec_point<T>* medoid, int beamSize,
+//                                     unsigned d) {
+//   // initialize data structures
+//   std::unordered_set<int> visited;
+//   auto q_comp = [] (const pid& l, const pid& r) {
+//     if (l.second < r.second) return true;
+//     if (l.second == r.second) return l.first < r.first;
+//     return false;
+//   };
+//   std::set<pid, decltype(q_comp)> frontier(q_comp);
+//   std::set<pid, decltype(q_comp)> beam(q_comp);
+
+//   auto ret = parlay::sequence<pid>();
+//   ret.reserve(beamSize);
+
+//   // the frontier starts with the medoid
+//   auto medoid_pair = std::make_pair(
+//       medoid->id,
+//       distance(medoid->coordinates.begin(), p->coordinates.begin(), d));
+//   frontier.insert(medoid_pair);
+//   beam.insert(medoid_pair);
+
+//   // terminate beam search when the entire frontier has been visited
+//   while (!frontier.empty()) {
+//     // the next node to visit is the unvisited frontier node that is closest to
+//     // p
+//     pid currentPid = *(frontier.begin());
+//     frontier.erase(frontier.begin());
+
+//     ret.push_back(currentPid);
+//     visited.insert(currentPid.first);
+
+//     Tvec_point<T>* current = v[currentPid.first];
+//     auto g = [&] (int ngh) {
+//       if (visited.find(ngh) != visited.end()) return false;
+//       return true;
+//     };
+
+//     // for (const int& ngh : current->out_nbh) {
+//     //   if (visited.find(ngh) == visited.end()) {
+//     //     float dist = distance(v[ngh]->coordinates.begin(), p->coordinates.begin(), d);
+//     //     // insert it into the beam
+//     //     beam.insert(std::make_pair(ngh, dist));
+//     //     frontier.insert(std::make_pair(ngh, dist));
+//     //     visited.insert(ngh);
+//     //   }
+//     // }
+
+//     for(int i=0; i<size_of(current->out_nbh); i++){
+//       int ngh = current->out_nbh[i];
+//       if (visited.find(ngh) == visited.end()) {
+//         float dist = distance(v[ngh]->coordinates.begin(), p->coordinates.begin(), d);
+//         // insert it into the beam
+//         beam.insert(std::make_pair(ngh, dist));
+//         frontier.insert(std::make_pair(ngh, dist));
+//         visited.insert(ngh);
+//       }
+//     }
+
+//     while (beam.size() > beamSize) {
+//       auto it = std::prev(beam.end());
+//       pid point = *it;
+//       beam.erase(it);
+//       auto f_it = frontier.find(point);
+//       if (f_it != frontier.end()) {
+//         frontier.erase(f_it);
+//       }
+//     }
+//   }
+//   return ret;
+// }
---
+++ examples/diskann/vamana/neighbors.h
@@ -0,0 +1,93 @@
+// This code is part of the Problem Based Benchmark Suite (PBBS)
+// Copyright (c) 2011 Guy Blelloch and the PBBS team
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights (to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#include <algorithm>
+#include "parlay/parallel.h"
+#include "parlay/primitives.h"
+#include "parlay/random.h"
+//#include "common/geometry.h"
+#include "../utils/NSGDist.h"  
+#include "../utils/types.h"
+#include "index.h"
+#include "../utils/beamSearch.h"  
+#include "../utils/indexTools.h"
+#include "../utils/stats.h"
+
+extern bool report_stats;
+
+template<typename T>
+void ANN(parlay::sequence<Tvec_point<T>*> &v, int k, int maxDeg, int beamSize, int beamSizeQ, double alpha, double dummy,
+  parlay::sequence<Tvec_point<T>*> &q) {
+  parlay::internal::timer t("ANN",report_stats); 
+  {
+    unsigned d = (v[0]->coordinates).size();
+    std::cout << "Size of dataset: " << v.size() << std::endl; 
+    using findex = knn_index<T>;
+    findex I(maxDeg, beamSize, alpha, d);
+
+    
+    int parts = 10; 
+    size_t n = v.size();
+    size_t m = (size_t) (n/parts);
+    size_t r = m/2; 
+    parlay::sequence<int> inserts = parlay::tabulate(n, [&] (size_t i){return static_cast<int>(i);});
+    std::cout << "Building with points " << inserts[inserts.size()-1] << " through " << inserts[0] << std::endl; 
+    I.build_index(v, inserts);
+
+    I.searchNeighbors(q, v, beamSizeQ, k);
+    t.next("Found nearest neighbors");
+    if(report_stats){
+      //average numbers of nodes searched using beam search
+      graph_stats(v);
+      query_stats(q);
+      t.next("stats");
+    }
+  };
+}
+
+
+template<typename T>
+void ANN(parlay::sequence<Tvec_point<T>*> v, int maxDeg, int beamSize, double alpha, double dummy) {
+  parlay::internal::timer t("ANN",report_stats); 
+  { 
+    unsigned d = (v[0]->coordinates).size();
+    std::cout << "Size of dataset: " << v.size() << std::endl; 
+    using findex = knn_index<T>;
+    findex I(maxDeg, beamSize, alpha, d);
+    I.build_index(v, parlay::tabulate(v.size(), [&] (size_t i){return static_cast<int>(i);}));
+    t.next("Built index");  
+    if(report_stats){
+      graph_stats(v);
+      t.next("stats");
+    }
+  };
+}
+
+
+    // int parts = 10;
+    // size_t m = (size_t) (v.size()/parts);
+    // for(int i=0; i<parts; i++){
+    //   parlay::sequence<int> delete_list = parlay::tabulate(m, [&] (size_t j){return static_cast<int>(parts*i+j);});
+    //   I.lazy_delete(delete_list, v);
+    //   I.consolidate_deletes(v);
+    //   I.batch_insert(delete_list, v, true);
+    // }
