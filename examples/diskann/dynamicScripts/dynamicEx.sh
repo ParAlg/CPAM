081b1f452d7e4df21dafe386fa029ab9470b314d
Magdalen Dobson <magdalen@aware.aladdin.cs.cmu.edu>
2022-08-04 22:37:42 -0400

    added another dynamic script

--- examples/diskann/dynamicScripts/dynamicEx.sh
+++ examples/diskann/dynamicScripts/dynamicEx.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+echo "BASELINE COMPARISON"
+cd ..
+make
+rm outFile
+./neighbors -R 100 -L 125 -k 200 -Q 250 -q /ssd1/ANN/sift/bigann_query.bvecs -o outFile /ssd1/ANN/sift/sift1M.bvecs
+cd bench
+./neighborsCheck -r [1,2,5,10,50,100,150,200] /ssd1/ANN/sift/idx_1M.ivecs ../outFile
+echo "CYCLE EXPERIMENT"
+cd ../dynamicScripts
 make clean all
 rm outFileC
 ./neighborsCycle -R 100 -L 125 -k 200 -Q 250 -q /ssd1/ANN/sift/bigann_query.bvecs -o outFileC /ssd1/ANN/sift/sift1M.bvecs
 cd ../bench
-./neighborsCheck -r [1,2,5,10,50,100,125,150,175,200] /ssd1/ANN/sift/idx_1M.ivecs ../dynamicScripts/outFileC
-# cd ../dynamicScripts
-# rm outFileS
-#neighbors command
-# cd ../bench
-#check command
+./neighborsCheck -r [1,2,5,10,50,100,150,200] /ssd1/ANN/sift/idx_1M.ivecs ../dynamicScripts/outFileC
+echo "SLIDING WINDOW EXPERIMENT"
+cd ../dynamicScripts
+rm outFileS
+./neighborsSlidingWindow -R 100 -L 125 -k 200 -Q 250 -q /ssd1/ANN/sift/bigann_query.bvecs -o outFileS /ssd1/ANN/sift/sift10M.bvecs
+cd ../bench
+./neighborsCheck -r [1,2,5,10,50,100,150,200] /ssd1/ANN/sift/idx_1M.ivecs ../dynamicScripts/outFileS
--- examples/diskann/dynamicScripts/neighborsSlidingWindow.cpp
+++ examples/diskann/dynamicScripts/neighborsSlidingWindow.cpp
@@ -31,20 +31,21 @@ void ANN(parlay::sequence<Tvec_point<T>*> v, int maxDeg, int beamSize,
     std::cout << "Size of dataset: " << v.size() << std::endl;
     using findex = knn_index<T>;
     findex I(v, maxDeg, beamSize, alpha, d);
-    timer build_t;
-    build_t.start();
-    I.build_index(parlay::tabulate(
-        v.size(), [&](size_t i) { return static_cast<node_id>(i); }));
-    build_t.next("Build time");
-    int parts = 20;
-    size_t m = v.size()/parts;
-    for(int i=0; i<20; i++){
-      parlay::sequence<node_id> indices = parlay::tabulate(m, [&] (size_t j){return static_cast<node_id>(i*m+j);});
-      std::cout << "Deleting indices " << indices[0] << " through " << indices[m-1] << std::endl; 
-      I.lazy_delete(indices);
+    int parts = 10; 
+    size_t n = v.size();
+    size_t m = (size_t) (n/parts);
+    size_t r = m/2; 
+    parlay::sequence<node_id> inserts = parlay::tabulate(m, [&] (size_t i){return static_cast<node_id>(n-i-1);});
+    std::cout << "Building with points " << inserts[inserts.size()-1] << " through " << inserts[0] << std::endl; 
+    I.build_index(inserts);
+    for(int i=parts*2-1; i>1; i--){
+      parlay::sequence<node_id> delete_list = parlay::tabulate(r, [&] (size_t j){return static_cast<node_id>(((i)*r)+r-j-1);});
+      std::cout << "Deleting points " << delete_list[delete_list.size()-1] << " through " << delete_list[0] << std::endl; 
+      I.lazy_delete(delete_list);
       I.consolidate_deletes();
-      std::cout << "Re-inserting " << indices[0] << " through " << indices[m-1] << std::endl; 
-      I.insert(indices);
+      parlay::sequence<node_id> insert_list = parlay::tabulate(r, [&] (size_t j){return static_cast<node_id>(((i)*r)-r-j-1);});
+      std::cout << "Inserting points " << insert_list[insert_list.size()-1] << " through " << insert_list[0] << std::endl; 
+      I.insert(insert_list);
     }
   };
 }
@@ -59,17 +60,22 @@ void ANN(parlay::sequence<Tvec_point<T>*> v, int maxDeg, int beamSize,
     std::cout << "Size of dataset: " << v.size() << std::endl;
     using findex = knn_index<T>;
     findex I(v, maxDeg, beamSize, alpha, d);
-    I.build_index(parlay::tabulate( v.size(), [&](size_t i) { return static_cast<node_id>(i); }));
-
-    int parts = 20;
-    size_t m = v.size()/parts;
-    for(int i=0; i<20; i++){
-      parlay::sequence<node_id> indices = parlay::tabulate(m, [&] (size_t j){return static_cast<node_id>(i*m+j);});
-      std::cout << "Deleting indices " << indices[0] << " through " << indices[m-1] << std::endl; 
-      I.lazy_delete(indices);
+    int parts = 10; 
+    size_t n = v.size();
+    size_t m = (size_t) (n/parts);
+    size_t r = m/2; 
+    std::cout << "Here" << std::endl;
+    parlay::sequence<node_id> inserts = parlay::tabulate(m, [&] (size_t i){return static_cast<node_id>(n-i-1);});
+    std::cout << "Building with points " << inserts[inserts.size()-1] << " through " << inserts[0] << std::endl; 
+    I.build_index(inserts);
+    for(int i=parts*2-1; i>1; i--){
+      parlay::sequence<node_id> delete_list = parlay::tabulate(r, [&] (size_t j){return static_cast<node_id>(((i)*r)+r-j-1);});
+      std::cout << "Deleting points " << delete_list[delete_list.size()-1] << " through " << delete_list[0] << std::endl; 
+      I.lazy_delete(delete_list);
       I.consolidate_deletes();
-      std::cout << "Re-inserting " << indices[0] << " through " << indices[m-1] << std::endl; 
-      I.insert(indices);
+      parlay::sequence<node_id> insert_list = parlay::tabulate(r, [&] (size_t j){return static_cast<node_id>(((i)*r)-r-j-1);});
+      std::cout << "Inserting points " << insert_list[insert_list.size()-1] << " through " << insert_list[0] << std::endl; 
+      I.insert(insert_list);
     }
 
     parlay::sequence<parlay::sequence<unsigned>> query_results(q.size());
