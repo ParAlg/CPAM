6106b66ac2e83dbc538e8b0a259374ea693d7299
Magdalen Dobson <magdalen@aware.aladdin.cs.cmu.edu>
2022-09-26 15:52:23 -0400

    commit so I can switch branches

--- examples/diskann/dynamicScripts/dynamicEx.sh
+++ examples/diskann/dynamicScripts/dynamicEx.sh
@@ -6,13 +6,13 @@ rm outFile
 ./neighbors -R 100 -L 125 -k 200 -Q 250 -q /ssd1/ANN/sift/bigann_query.bvecs -o outFile /ssd1/ANN/sift/sift1M.bvecs
 cd bench
 ./neighborsCheck -r [1,2,5,10,50,100,150,200] /ssd1/ANN/sift/idx_1M.ivecs ../outFile
-echo "CYCLE EXPERIMENT"
-cd ../dynamicScripts
-make clean all
-rm outFileC
-./neighborsCycle -R 100 -L 125 -k 200 -Q 250 -q /ssd1/ANN/sift/bigann_query.bvecs -o outFileC /ssd1/ANN/sift/sift1M.bvecs
-cd ../bench
-./neighborsCheck -r [1,2,5,10,50,100,150,200] /ssd1/ANN/sift/idx_1M.ivecs ../dynamicScripts/outFileC
+# echo "CYCLE EXPERIMENT"
+# cd ../dynamicScripts
+# make clean all
+# rm outFileC
+# ./neighborsCycle -R 100 -L 125 -k 200 -Q 250 -q /ssd1/ANN/sift/bigann_query.bvecs -o outFileC /ssd1/ANN/sift/sift1M.bvecs
+# cd ../bench
+# ./neighborsCheck -r [1,2,5,10,50,100,150,200] /ssd1/ANN/sift/idx_1M.ivecs ../dynamicScripts/outFileC
 # echo "SLIDING WINDOW EXPERIMENT"
 # cd ../dynamicScripts
 # rm outFileS
